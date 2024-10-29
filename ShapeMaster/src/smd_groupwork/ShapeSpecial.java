5c7ac757bf77fc2cbfcc82d38b60f56997a238e8
UmerAsif-1 <156857300+UmerAsif-1@users.noreply.github.com>
2024-10-29 18:24:51 +0200

    ShapeSpecial is an interface, any class that implements it will need to provide its own specific implementations for each of these methods.

--- ShapeMaster/src/smd_groupwork/ShapeSpecial.java
+++ ShapeMaster/src/smd_groupwork/ShapeSpecial.java
@@ -1,5 +1,12 @@
 package smd_groupwork;
 
 public interface ShapeSpecial {
+	
+	// calculate the area of a shape,implemented by concrete classes
+	double calculateArea();
+	// calculate the perimeter of a shape,implemented by concrete classes
+	double calculatePerimeter();
+	// draw the shape, to be implemented by concrete classes
+	void drawShape();
 
 }
