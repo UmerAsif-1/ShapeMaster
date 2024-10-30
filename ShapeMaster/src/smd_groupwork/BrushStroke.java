9781ffdb53e04813ecd246db9339f4b0bc5531d5
UmerAsif-1 <156857300+UmerAsif-1@users.noreply.github.com>
2024-10-30 08:35:37 +0200

    Implemented BrushStroke class with color and thickness attributes

---
+++ ShapeMaster/src/smd_groupwork/BrushStroke.java
@@ -0,0 +1,43 @@
+package smd_groupwork;
+
+import java.util.Random;
+
+public class BrushStroke extends Visibility implements ShapeBasic, CanBeColored {
+	private final int thickness;
+	private  String color = "Yellow";
+	
+	public BrushStroke() {
+		Random random = new Random();
+	    this.thickness = random.nextInt(10) + 1; // range 1-10
+	    }
+
+	@Override
+    public void draw() {
+        System.out.println("Brush strokes with thickness " + thickness);
+    }
+
+    @Override
+    public void grow() {
+        System.out.println("Adding some color... " + getColor());
+    }
+
+    @Override
+    public String printText() {
+        return "Brush Stroke";
+    }
+
+    @Override
+    public String setColor(String newColor) {
+        this.color = newColor;
+        return this.color;
+    }
+
+    @Override
+    public String getColor() {
+        if ("No color".equals(color)) {
+            return "Out of color...";
+        } else {
+            return color;
+        }
+    }
+}
